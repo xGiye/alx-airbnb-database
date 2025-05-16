37bdfd3a424b16087fbb65491887dfcfcc3ac896
TechyBarry <amadionengiyewilliams@gmail.com>
2025-05-16 13:20:56 +0100

    Aggregations and window_function

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -17,13 +17,20 @@ ORDER BY
 
 -- A SQL query that uses the RANK() window function to rank properties based on the total number of bookings
 SELECT 
-    p.property_id,
-    p.name AS property_name,
-    COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
-FROM 
-    Property p
-LEFT JOIN 
-    Booking b ON p.property_id = b.property_id
-GROUP BY 
-    p.property_id, p.name;
+    property_id,
+    property_name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number_rank,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank
+FROM (
+    SELECT 
+        p.property_id,
+        p.name AS property_name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM 
+        Property p
+    LEFT JOIN 
+        Booking b ON p.property_id = b.property_id
+    GROUP BY 
+        p.property_id, p.name
+) AS property_bookings;
